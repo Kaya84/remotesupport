dcd26d4ce6abd0d7ae3ba1c3846591a3a3533c70
Kaya84 <35736369+Kaya84@users.noreply.github.com>
2021-11-19 17:41:28 +0100

    Update setup.php
    Commented class until config page works properly

--- setup.php
+++ setup.php
@@ -34,10 +34,10 @@ function plugin_init_remotesupport()
 		
       $PLUGIN_HOOKS['add_javascript']['remotesupport'][] = 'js/support.js';
       // Add Config Page
-      Plugin::registerClass('PluginRemotesupportConfig', ['addtabon' => 'Config']);
+//      Plugin::registerClass('PluginRemotesupportConfig', ['addtabon' => 'Config']);
 	  
-		$PLUGIN_HOOKS["menu_toadd"]['remotesupport'] = ['tools'  => 'PluginRemotesupportMenu'];
-		$PLUGIN_HOOKS['config_page']['remotesupport'] = 'front/index.php';
+//		$PLUGIN_HOOKS["menu_toadd"]['remotesupport'] = ['tools'  => 'PluginRemotesupportMenu'];
+//		$PLUGIN_HOOKS['config_page']['remotesupport'] = 'front/index.php';
 		$PLUGIN_HOOKS['menu']['remotesupport']                     = true;
 		$PLUGIN_HOOKS['post_init']['remotesupport'] =   'plugin_remotesupport_postinit';
 
