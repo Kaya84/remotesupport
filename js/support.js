eb42551f89b414196d56639566277c4ca1a4324b
Kaya84 <35736369+Kaya84@users.noreply.github.com>
2021-11-22 15:20:04 +0100

    Update support.js
    Fix when computer title has spaces.
    Temporary fix: will work better with next release

--- js/support.js
+++ js/support.js
@@ -3,8 +3,8 @@ $(document).on('ready', function() {
 
 //commento di check se carica
 	if ($(location).attr('href').includes('computer') ){
-		name = $(".navigationheader").find("span").first().text().trim();
-		u = "<span><a href='vnc://" + name +"'> Collegati con VNC</a><br><a href='rdp://" + name +"'> Collegati con RDP</a></span>";
+		name =  $(".navigationheader").find("span").first().text().split(" ")[0].trim();
+		u = "<span><a href='vnc://" + name +"'>VNC Connection</a><br><a href='rdp://" + name +"'>RDP Connection</a></span>";
 		$(".navigationheader").find("span").first().after(u);
 	}
 
