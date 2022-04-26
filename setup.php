1d1b7f8e881785787d6c8c1f7e1ad710f4b5fcbc
Kaya84 <35736369+Kaya84@users.noreply.github.com>
2022-04-26 11:27:31 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -7,17 +7,17 @@
 
  -------------------------------------------------------------------------
  LICENSE
- This file is part of Camera Input.
- Camera Input is free software; you can redistribute it and/or modify
+ This file is part of Remote Support.
+ Remote support is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
- Camera Input is distributed in the hope that it will be useful,
+ Remote support is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
  You should have received a copy of the GNU General Public License
- along with Camera Input. If not, see <http://www.gnu.org/licenses/>.
+ along with Remote support. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
 */
 
